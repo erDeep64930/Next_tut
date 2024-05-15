b6d2fb9d662feafff2f4271d323327cc3e5a79c6
erDeep640390 <erdeep64930@gmail.com>
2024-05-15 14:58:22 +0530

    navbar has been creqated

---
+++ constant/constant.jsx
@@ -0,0 +1,7 @@
+export const menuItem = [
+    {id:1, title:"Home", path:"/"},
+    {id:2, title:"Contact", path:"/contact"},
+    {id:3, title:"About", path:"/about"},
+    {id:4, title:"Products", path:"/products"},
+   
+]
